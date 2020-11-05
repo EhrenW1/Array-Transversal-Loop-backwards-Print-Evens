4b2f7acf58c2d88637890ff5784b17f81979ee42
EhrenW1 <ehren.wargo@patelhs.org>
2020-11-05 20:48:08 +0000

    Ran and saw code worked

--- Main.java
+++ Main.java
@@ -6,7 +6,7 @@ int[] numbers = {1,2,3,4,5,6,7,8,9,10};
 for(int i = 9; i >= 0; i--){
    if(numbers[i] % 2 == 0){ 
 
-    System.out.println(numbers{i});
+    System.out.println(numbers[i]);
     }
    }
   }
